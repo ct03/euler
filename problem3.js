8f55c92d1d106354be34bfe68412ff80bf6b9cde
ct03 <cptra.n@gmail.com>
2017-03-16 16:40:47 -0500

    Add files via upload

--- problem3.js
+++ problem3.js
@@ -1,26 +1,37 @@
 function findFactorals(x){
-	for (i=2;i<x;i++){
-		if(x%i === 0 && findPrime(i)){console.log(i);}
-	} 
-}
-
-function findPrime(num){
-	for (i=2;i<num;i++) {
-		if(num%i === 0){return false;}
-	} return num;
-}
-
-
-
-//solution  - above stuff is notes
-function findFactorals(x){
 	for (i=x-1;i>1;i--){
-		if(x%i === 0 && findPrime(i)){console.log(i);}
+		if(x%i === 0 && findPrime(i)){console.log(i);break;}
 	} 
 }
 
 function findPrime(num){
-	for (z=num-1;z>1;z--) {
-		if(num%z === 0){return false;}
+
+	for (y=num-1;y>1;y--) {
+
+	if(num%y === 0){return false;}
+
 	} return num;
+
 }
+
+findFactorals(600851475143); 
+
+//factoral ascending
+for(i=2;i<euler;i++){
+  if(euler%i === 0){
+    console.log(i);	
+    break;
+  }
+}
+
+//factoral descending
+for(i=euler-1;i>1;i--){
+  if(euler%i === 0){
+    console.log(i);
+    break;
+  }
+}
+
+function findEuler(x){
+  for (i=2;i<x;i++){
+    if (x%i===0){console.log(i); break;}}}
